7b22b489423fb5ca18201f069b217f75e9164c96
Raushan Kumar <Er.raushanjha@gmail.com>
2024-05-08 18:55:13 +0530

    Resturant info detail

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -1,26 +1,60 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function RestaurantItem() {
   return (
     <View>
-      <Text>RestaurantItem</Text>
+      
       {/* Resturant image*/}
       <RestaurentImage />
       {/* Resturant Info*/}
+      <RestaurentInfo />
     </View>
   );
 }
 
 const RestaurentImage = () => (
   <>
-
-  <Image
-    source={{
-      uri: "https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg",
-    }}
-   
-    style={{ width: "100%", height: 180 }}
-  />
+    <Image
+      source={{
+        uri: "https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg",
+      }}
+      style={{ width: "100%", height: 180 }}
+    />
+    <TouchableOpacity style={{ position: "absolute", right: 20, top: 20 }}>
+      <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
+    </TouchableOpacity>
   </>
+);
+
+const RestaurentInfo = () => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+        Farmehouse kitchen thai cuisine
+      </Text>
+      <Text style={{ fontSize: 13, color: "gray" }}>40-45 mins.</Text>
+    </View>
+    <View
+      style={{
+        backgroundColor: "#eee",
+        height: 30,
+        width: 30,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 15,
+      }}
+    >
+      <Text>4.5</Text>
+    </View>
+  </View>
 );
